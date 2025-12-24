8a91428e4df65305686fa9660393407455b86de7
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-24 13:34:36 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,16 @@
 import WalletConnect from '@/components/ConnectButton';
 import MessageBoard from '@/components/MessageBoard';
+import MessageList from '@/components/MessageList';
 
 export default function Home() {
   return (
-    <main className="max-w-xl mx-auto mt-20 space-y-6">
+    <main className="max-w-xl mx-auto mt-16 space-y-6">
       <h1 className="text-3xl font-bold text-center">
         Base Message Board
       </h1>
-
       <WalletConnect />
       <MessageBoard />
+      <MessageList />
     </main>
   );
 }
