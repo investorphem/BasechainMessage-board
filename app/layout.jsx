fd113325fd40caa0b6e15523de88ab89618fdf5d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-26 23:09:53 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,26 @@
 'use client';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { wagmiConfig } from '@/lib/wagmi';
 import '@rainbow-me/rainbowkit/styles.css';
-import './globals.css'; // Ensure your CSS is imported
+import './globals.css';
 
-// Create the query client outside the component to prevent recreation on re-renders
+// Create query client once
 const queryClient = new QueryClient();
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Domain Verification */}
+        <meta
+          name="talentapp:project_verification"
+          content="3b7f9f317de9a4de5f152edfee9518ed647e99c84d404b66292c415dc7c6eb883f08294b9c2b4992d48ae445cde0e2bbb1f999afaf0a12933b0f5cf42eb37938"
+        />
+      </head>
+
       <body>
         <WagmiProvider config={wagmiConfig}>
           <QueryClientProvider client={queryClient}>
